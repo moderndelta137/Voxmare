2e5dec29a553b7f7e124413b8c3d7c92082f91f9
Atsu30 <atsmr.30autb@gmail.com>
2020-07-07 01:32:09 +0900

    Make Standard Block and Attack Block

---
+++ Voxmare_project/Assets/Mori/Script/Block.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Block : MonoBehaviour
+{
+    public enum BlockType
+    {
+        STANDARD,
+        ATTACK
+    }
+
+    // Variable
+    [SerializeField] public BlockParameter param;
+
+    int id;
+    BlockType blockType;
+    List<int> pairs;        // store pairs by block's id
+
+    // Method
+
+    /// <summary>
+    /// This function link this block to target block.
+    /// </summary>
+    /// <param name="block">target block</param>
+    void LinkBlockTo(Block block)
+    {
+        if(!this.CheckLinkable())
+        {
+            Debug.Log("This Block is full. [ID : " + this.id + " ]");
+            return;
+        }
+
+        if(!block.CheckLinkable())
+        {
+            Debug.Log("Target Block is full. [ID : " + block.id + " ]");
+            return;
+        }
+
+        this.pairs.Add(block.id);
+        block.pairs.Add(this.id);
+    }
+
+    bool CheckLinkable()
+    {
+        return pairs.Count < param.maxPairs;
+    }
+
+    protected void Start()
+    {
+        pairs = new List<int>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Voxmare_project/Assets/Mori/Script/BlockParameter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/BlockParameter", fileName = "BlockParameter")]
+public class BlockParameter : ScriptableObject
+{
+    public int maxPairs = 4;
+    public int blockPower = 1;
+    public float blockCoolTime = 1.0f;
+    public List<Block.BlockType> linkableBlockType;
+}
