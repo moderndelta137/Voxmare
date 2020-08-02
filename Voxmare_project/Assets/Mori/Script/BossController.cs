8e63bc32c7e42cb7d9c79de206f8341139bd8d2b
Atsu30 <atsmr.30autb@gmail.com>
2020-08-02 17:46:53 +0900

    Add spin attack in boss AI

--- Voxmare_project/Assets/Mori/Script/BossController.cs
+++ Voxmare_project/Assets/Mori/Script/BossController.cs
@@ -18,23 +18,32 @@ public class BossController : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] public AnimationCurve centripetalPower;
     [Header("Tackle")]
-    [SerializeField] float interval;
-    [SerializeField] float intervalDivergence;
+    [SerializeField] float tackleInterval;
+    [SerializeField] float tackleIntervalDivergence;
     [SerializeField] float tackleDuration;
     [SerializeField] Ease tackleEase;
+    [Header("Spin")]
+    [SerializeField] float spinInterval;
+    [SerializeField] float spinIntervalDivergence;
+    [SerializeField] float spinDuration;
+    [SerializeField] int spinCount;
+    [SerializeField] Ease spinEase;
     [Header("CalculateCenter")]
     [SerializeField] float calculateCenterInterval;
 
     Vector3 detination;
+    List<Vector3> paths;
     Tween tackleTween;
+    Sequence spinSequence;
     WaitForSeconds waitTackleInterval;
+    WaitForSeconds waitSpinInterval;
     WaitForSeconds waitCalculateCenterInterval;
 
     enum State
     {
         Random,
         Tackle,
-        Leave
+        Spin
     }
 
     void Start()
@@ -43,10 +52,13 @@ public class BossController : MonoBehaviour
         randomSeed = Random.value;
         state = State.Random;
 
-        waitTackleInterval = new WaitForSeconds(interval + Random.Range(-intervalDivergence, intervalDivergence));
+        paths = new List<Vector3>();
+        waitTackleInterval = new WaitForSeconds(tackleInterval + Random.Range(-tackleIntervalDivergence, tackleIntervalDivergence));
+        waitSpinInterval = new WaitForSeconds(spinInterval + Random.Range(-spinIntervalDivergence, spinIntervalDivergence));
         waitCalculateCenterInterval = new WaitForSeconds(calculateCenterInterval);
-
+        spinSequence = DOTween.Sequence();
         StartCoroutine("Tackle");
+        StartCoroutine("Spin");
         StartCoroutine("CalculateCenter");
     }
 
@@ -60,7 +72,7 @@ public class BossController : MonoBehaviour
                 break;
             case State.Tackle:
                 break;
-            case State.Leave:
+            case State.Spin:
                 break;
             default:
                 break;
@@ -82,9 +94,33 @@ public class BossController : MonoBehaviour
         while (true)
         {
             yield return waitTackleInterval;
+            if (state != State.Random) continue;
+
             state = State.Tackle;
             tackleTween = transform.DOMove(target.transform.position, tackleDuration).SetEase(tackleEase);
             yield return tackleTween.WaitForCompletion();
+            state = State.Random;
+        }
+    }
+
+    private IEnumerator Spin()
+    {
+        int divideCount = 8;
+        int rand = Random.Range(0, divideCount);
+        for (int i = 0; i < divideCount; i++)
+        {
+            float angle = ((i + rand) % divideCount) * (2 * Mathf.PI / divideCount);
+            paths.Add(new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle)));
+        }
+
+        while (true)
+        {
+            yield return waitSpinInterval;
+            if (state != State.Random) continue;
+            state = State.Spin;
+            spinSequence.Join(transform.DOLocalPath(paths.ToArray(), spinDuration, PathType.CatmullRom).SetEase(spinEase));
+            spinSequence.Join(transform.DOBlendableLocalRotateBy(new Vector3(0, 360*spinCount, 0), spinDuration, RotateMode.FastBeyond360).SetEase(spinEase));
+            yield return spinSequence.WaitForCompletion();
             state = State.Random;
         }
     }
@@ -94,6 +130,8 @@ public class BossController : MonoBehaviour
         while(true)
         {
             yield return waitCalculateCenterInterval;
+            if (state != State.Random) continue;
+
             Vector3 sum = Vector3.zero;
             foreach (Transform block in transform)
             {
@@ -113,7 +151,7 @@ public class BossController : MonoBehaviour
             transform.position = blocksCenter;
             foreach (Transform block in transform)
             {
-                block.localPosition += -posToCenter;
+                block.localPosition += transform.rotation * -posToCenter;
             }
         }
     }
