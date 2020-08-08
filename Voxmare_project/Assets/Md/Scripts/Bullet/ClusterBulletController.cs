04efa9d4bf9a7496bb898f9b7beb5d6a98f2fdaf
Atsu30 <atsmr.30autb@gmail.com>
2020-08-08 20:33:51 +0900

    Cluster Effect

--- Voxmare_project/Assets/Md/Scripts/Bullet/ClusterBulletController.cs
+++ Voxmare_project/Assets/Md/Scripts/Bullet/ClusterBulletController.cs
@@ -9,25 +9,25 @@ public class ClusterBulletController : MonoBehaviour
     public float Lifespan;
     public float Radius;
     public bool Damage_player;
-    public Color Enemy_color;
-    public Color Player_color;
+    public ParticleSystem.MinMaxGradient Enemy_color;
+    public ParticleSystem.MinMaxGradient Player_color;
     private int layerMask;
     private RaycastHit[] hits;
-    private VisualEffect vfx; 
+    private ParticleSystem particle;
     // Start is called before the first frame update
     void Start()
     {
-        vfx = this.GetComponent<VisualEffect>();
-
+        particle = GetComponentInChildren<ParticleSystem>();
+        var colorOverLifetime = particle.colorOverLifetime;
         Destroy(this.gameObject, Lifespan);
         if(Damage_player)
         {
-            vfx.SetVector4("Color",Enemy_color);
+            colorOverLifetime.color = Enemy_color;
             layerMask = 1 << 8;//Check Player Layer
         }
         else
         {
-            vfx.SetVector4("Color",Player_color);
+            colorOverLifetime.color = Player_color;
             layerMask = 1 << 9;//Check Enemy Layer
         }
         hits=Physics.SphereCastAll(this.transform.position, Radius, Vector3.up, 0, layerMask);
