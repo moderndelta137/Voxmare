05d67314e106c2103a301b048de582529a87c935
mdelta137 <moderndelta137@gmail.com>
2020-08-10 00:46:56 +0900

    Update Main Menu
    ・Polished main menu graphic

---
+++ Voxmare_project/Assets/Md/Scripts/UI/BubbleAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class BubbleAnimation : MonoBehaviour
+{
+    public float move_strength;
+    private float move_duration;
+    public Vector2 move_duration_range;
+    private Vector3 move_direction;
+    private Tween myTween;
+    private Vector3 origin;
+    private IEnumerator myCoroutine;
+    //public RectTransform
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator StartMoving()
+    {
+        
+        //this.transform.DOShakePosition(move_duration,move_strength,vibrato).SetLoops(-1);
+        move_direction.x = Random.Range(-1f,1f);
+        move_direction.y = Random.Range(-1f,1f);
+        move_direction.Normalize();
+        move_duration = Random.Range(move_duration_range.x,move_duration_range.y);
+        myTween = this.transform.DOMove(this.transform.position+move_direction*move_strength,move_duration).SetLoops(2, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        yield return myTween.WaitForCompletion();
+         StartCoroutine(StartMoving());
+    }
+
+    private void OnDisable() 
+    {
+        StopAllCoroutines();
+        DOTween.KillAll();
+        this.transform.position = origin;     
+    }
+
+    private void OnEnable() 
+    {
+        myCoroutine = StartMoving();
+        StartCoroutine(myCoroutine);
+    }
+}
---
+++ Voxmare_project/Assets/Md/Scripts/UI/TitleEyeAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleEyeAnimation : MonoBehaviour
+{
+    public float movement_clamp;
+    public float movement_scale;
+    private Vector3 origin;
+    private Vector3 mouse_position;
+    public Camera UI_camera;
+    private Vector3 movement_direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = this .transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse_position.x = Input.mousePosition.x;
+        mouse_position.y = Input.mousePosition.y;
+        mouse_position.z = 0;
+        Debug.Log(UI_camera.WorldToScreenPoint(this.transform.position));
+        movement_direction = UI_camera.WorldToScreenPoint(this.transform.position);
+        movement_direction.z = 0;
+        movement_direction = mouse_position - movement_direction;
+        movement_direction = movement_direction * movement_scale;
+        Vector3.ClampMagnitude(movement_direction,movement_clamp);
+
+        this.transform.localPosition = origin + movement_direction;
+    }
+}
