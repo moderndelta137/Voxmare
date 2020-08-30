4735fde94507efeddb7be6c25aa790ec9d0246ca
Atsu30 <atsmr.30autb@gmail.com>
2020-08-30 15:32:47 +0900

    Update Bullets : Simplified Ice and Rocket Bullet

---
+++ Voxmare_project/Assets/Mori/Script/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOLocalRotateQuaternion(Quaternion.LookRotation(-transform.forward), duration).SetLoops(-1, LoopType.Incremental);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
