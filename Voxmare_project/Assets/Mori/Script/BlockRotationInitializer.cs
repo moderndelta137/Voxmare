38091fd51e8b1a22255d261d33453aa133c5a719
Atsu30 <atsmr.30autb@gmail.com>
2020-09-29 07:03:39 +0900

    Decide Standard block's rotation first randomly

---
+++ Voxmare_project/Assets/Mori/Script/BlockRotationInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockRotationInitializer : MonoBehaviour
+{
+    void Start()
+    {
+        switch (Random.Range(0, 6))
+        {
+            case 0:
+                transform.rotation = Quaternion.LookRotation(transform.forward, transform.up);
+                break;
+            case 1:
+                transform.rotation = Quaternion.LookRotation(transform.forward, transform.right);
+                break;
+            case 2:
+                transform.rotation = Quaternion.LookRotation(transform.forward, -transform.up);
+                break;
+            case 3:
+                transform.rotation = Quaternion.LookRotation(transform.forward, -transform.right);
+                break;
+            case 4:
+                transform.rotation = Quaternion.LookRotation(transform.up, -transform.forward);
+                break;
+            case 5:
+                transform.rotation = Quaternion.LookRotation(-transform.up, transform.forward);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
